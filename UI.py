15aeea84b52bad5db3ebb54e40fe4d782ef1aff3
wahlstrs <48026272+wahlstrs@users.noreply.github.com>
2022-07-11 21:57:49 -0600

    Completed add and delete functionality for plants in Table, and added return to main menu

--- UI.py
+++ UI.py
@@ -10,43 +10,70 @@ class Plant():
         self.fert = fert
 
 class Table():
-    def __init__(self, root):
+    def __init__(self, root, window):
+        self.window= window
         self.root =root
-        self.plants = []
-        self.row_index = 2
+        self.plants = {}
+        self.row_index = 6
         self.e = Label(root, text = "My Plant List", width=10, font = ("Helvetica", "16", "bold italic"))
-        self.e.grid(row=0, column=2)
+        self.e.grid(row=2, column=2)
+        self.button= Button(self.root, text= "Click To Exit And Return To Main Menu", command=self.go_back,font = ("Helvetica", "8", "bold italic") )
+        self.button.grid(row=0, column=0)
         headers = ["Plant", "Next Water Date", "Next Fertilize Date", "Mark Watered", "Mark Fertilized"]
         for i in range(len(headers)):
             self.e = Label(root,text= headers[i], width= 15, font = ("Helvetica", "10", "bold"), relief='ridge')
-            self.e.grid(row=1, column=i, sticky ="nsew")
+            self.e.grid(row=5, column=i, sticky ="sew")
 
     def add_row(self, name, water, fert):
-        self.plants.append(Plant(name, water, fert))
-        self.name = Label(self.root, text = name)
-        self.name.grid(row=self.row_index, column=0, sticky='nsew')
-        self.row_index+=2
+        plant = Label(self.root, text = name )
+        plant.grid(row=self.row_index, column=0, sticky='nsew')
+        row = self.row_index-2
+        delete= Button(self.root, text =  f'Delete {name}', command= lambda: self.delete_row(row), font = ("Helvetica", "10", "bold"))
+        delete.grid(row=self.row_index, column=5, sticky='nsew')
+        self.plants[row]=(plant, delete)
+        self.row_index += 1
+
+    def delete_row(self, row_number):
+        """"""
+        if self.plants:
+            row = self.plants[row_number]
+            for item in row:
+                item.destroy()
+            del self.plants[row_number]
+
+    def go_back(self):
+        self.window.destroy()
+        master = Tk()
+        master.geometry("600x600")
+        f = Frame(master)
+        l = Label(f, text="Click here to create a new Plant List and start the Application",
+                  font=("Helvetica", "14", "bold italic"))
+        open_table = Button(f, text="Click here", command=lambda: make_list(master), font=("Helvetica", "10", "bold"))
+        f.pack(expand=True)
+        l.pack()
+        open_table.pack(padx=10, pady=10)
+
 
 
 
 class Add_Plant():
     def __init__(self, root, plant_grid):
         self.plant_grid = plant_grid
-        self.e = Label(root, text="Add Plant", font = ("Helvetica", "16", "bold italic"))
+        self.e = Label(root, text="Add Plant", font = ("Helvetica", "15", "bold italic"))
         self.e.pack(side=LEFT)
-        self.e = Label(root, text="Enter Plant Name")
+        self.e = Label(root, text="Enter Plant Name", anchor="w")
         self.plant_name = Entry(root, width =15)
         self.e.pack()
         self.plant_name.pack()
-        self.e = Label(root, text="Enter Watering Interval in Days")
+        self.e = Label(root, text="Enter Watering Interval in Days", anchor="w")
         self.watering = Entry(root, width =15)
         self.e.pack()
         self.watering.pack()
-        self.e = Label(root, text="Enter Fertilizing Interval in Days")
+        self.e = Label(root, text="Enter Fertilizing Interval in Days", anchor="w")
         self.fertilizing = Entry(root, width=15)
         self.e.pack()
         self.fertilizing.pack()
-        self.submit = Button(root,text="Submit", command = self.get_text)
+        self.submit = Button(root,text="Submit", command = self.get_text, font=("Helvetica", "10", "bold"))
         self.submit.pack(padx = 10, pady =10)
 
     def get_text(self):
@@ -59,14 +86,16 @@ class Add_Plant():
         self.fertilizing.delete(0, END)
 
 
-def make_list():
+def make_list(window):
     """"""
-    master.destroy()
+    window.destroy()
     root = Tk()
-    root.geometry("700x700")
+    root.wm_attributes('-toolwindow', 'True')
+    root.title('Plant Manager')
+    root.geometry("800x800")
     grid_frame = Frame(root)
     add_frame = Frame(root, highlightbackground="#698B69", highlightthickness=2, width= 300, height =300)
-    t= Table(grid_frame)
+    t= Table(grid_frame, root)
     a = Add_Plant(add_frame, t)
     border = LabelFrame(root, bd = 5, relief = SUNKEN, bg= "#698B69")
     border.pack()
@@ -76,10 +105,12 @@ def make_list():
     return
 
 master = Tk()
-master.geometry("400x400")
+master.wm_attributes('-toolwindow', 'True')
+master.title('Plant Manager')
+master.geometry("600x600")
 f = Frame(master)
-l = Label(f, text="Click here to create a new Plant List and Start Application", font = ("Helvetica", "10", "bold italic"))
-open_table = Button(f, text ="Click here", command=make_list, font = ("Helvetica", "10", "bold"))
+l = Label(f, text="Click here to create a new Plant List and start the Application", font = ("Helvetica", "14", "bold italic"))
+open_table = Button(f, text ="Click here", command=lambda: make_list(master), font = ("Helvetica", "10", "bold"))
 f.pack(expand=True)
 l.pack()
 open_table.pack(padx=10,pady=10)
