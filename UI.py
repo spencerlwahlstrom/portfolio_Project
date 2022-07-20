1d7652bb4bef5e525b9a367303c0ef25ff058cbd
wahlstrs <48026272+wahlstrs@users.noreply.github.com>
2022-07-20 13:04:06 -0600

    popup delete confirm window

--- UI.py
+++ UI.py
@@ -3,47 +3,64 @@
 # Description:
 from tkinter import *
 
-class Plant():
+
+class Plant:
     def __init__(self, name, water, fert):
         self.name = name
         self.water = water
         self.fert = fert
 
-class Table():
+
+class Table:
     def __init__(self, root, window):
-        self.window= window
-        self.root =root
+        self.window = window
+        self.root = root
         self.plants = {}
         self.row_index = 6
-        self.e = Label(root, text = "My Plant List", width=10, font = ("Helvetica", "16", "bold italic"))
+        self.e = Label(root, text="My Plant List", width=10, font=("Helvetica", "16", "bold italic"))
         self.e.grid(row=2, column=2)
-        self.button= Button(self.root, text= "Click To Exit And Return To Main Menu", command=self.go_back,font = ("Helvetica", "8", "bold italic") )
+        self.button = Button(self.root, text="Click To Exit And Return To Main Menu", command=self.go_back,font = ("Helvetica", "8", "bold italic") )
         self.button.grid(row=0, column=0)
         headers = ["Plant", "Next Water Date", "Next Fertilize Date", "Mark Watered", "Mark Fertilized"]
         for i in range(len(headers)):
             self.e = Label(root,text= headers[i], width= 15, font = ("Helvetica", "10", "bold"), relief='ridge')
-            self.e.grid(row=5, column=i, sticky ="sew")
+            self.e.grid(row=5, column=i, sticky ="nsew")
 
     def add_row(self, name, water, fert):
         plant = Label(self.root, text = name )
         plant.grid(row=self.row_index, column=0, sticky='nsew')
         row = self.row_index-2
-        delete= Button(self.root, text =  f'Delete {name}', command= lambda: self.delete_row(row), font = ("Helvetica", "10", "bold"))
+        delete= Button(self.root, text =  f'Delete {name}', command= lambda: self.popup_delete(row, name), font = ("Helvetica", "10", "bold"))
         delete.grid(row=self.row_index, column=5, sticky='nsew')
         self.plants[row]=(plant, delete)
         self.row_index += 1
 
-    def delete_row(self, row_number):
+    def popup_delete(self, row, name):
+        popup = Toplevel(self.root)
+        popup.wm_attributes('-toolwindow', 'True')
+        popup.title('Plant Manager')
+        popup.geometry("600x200")
+        l = Label(popup, text=f'Are you sure to want to delete this plant, {name}? Click confirm to delete, and cancel to go back', font=("Helvetica", "9", "bold italic"))
+        l.pack()
+        confirm = Button(popup,text="Confirm", command=lambda: self.delete_row(row, popup))
+        cancel = Button(popup, text="Cancel", command=popup.destroy)
+        confirm.pack()
+        cancel.pack()
+
+    def delete_row(self, row_number, window):
         """"""
         if self.plants:
             row = self.plants[row_number]
             for item in row:
                 item.destroy()
             del self.plants[row_number]
+        window.destroy()
 
     def go_back(self):
         self.window.destroy()
         master = Tk()
+        master.wm_attributes('-toolwindow', 'True')
+        master.title('Plant Manager')
         master.geometry("600x600")
         f = Frame(master)
         l = Label(f, text="Click here to create a new Plant List and start the Application",
@@ -60,6 +77,8 @@ class Add_Plant():
     def __init__(self, root, plant_grid):
         self.plant_grid = plant_grid
         self.e = Label(root, text="Add Plant", font = ("Helvetica", "15", "bold italic"))
+        self.e.pack(side=LEFT)
+        self.e = Label(root, text="Add a plant to My Plant List, \n to set watering and fertilizing \n intervals and to automatically \nreceive reminders however often\n  you choose!", font=("Helvetica", "8", "bold italic"))
         self.e.pack(side=LEFT)
         self.e = Label(root, text="Enter Plant Name", anchor="w")
         self.plant_name = Entry(root, width =15)
@@ -92,15 +111,15 @@ def make_list(window):
     root = Tk()
     root.wm_attributes('-toolwindow', 'True')
     root.title('Plant Manager')
-    root.geometry("800x800")
+    root.geometry("900x900")
     grid_frame = Frame(root)
     add_frame = Frame(root, highlightbackground="#698B69", highlightthickness=2, width= 300, height =300)
     t= Table(grid_frame, root)
     a = Add_Plant(add_frame, t)
     border = LabelFrame(root, bd = 5, relief = SUNKEN, bg= "#698B69")
     border.pack()
+    add_frame.pack(side=TOP, fill=BOTH, padx=10, pady=10)
     grid_frame.pack()
-    add_frame.pack(side=BOTTOM, fill=BOTH, padx = 10, pady =10)
     root.mainloop()
     return
 
